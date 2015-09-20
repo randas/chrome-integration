d50e49260ab847b9598c23fc03fba4eedf07a2b4
lkuprenas <luke@kuprenas.com>
2015-09-19 23:04:32 -0400

    still working

--- content.js
+++ content.js
@@ -1,28 +1,43 @@
-var para = document.createElement("p");
-var node = document.createTextNode("RED PANDAAAA");
-para.appendChild(node);
+var uberClientID = "1SgYLxvfNCPyUqS5s7uYZfB6AZlhdg49"
+	, serverToken = "Ks8Pt2MM9PGzf8WBW2ThZGZPmhBxFfkycbUSgyMk"
+	, gooToken = "AIzaSyCJhFwZi2GD3Qdsg7yiOLTv7cFa73FpDxI";
 
-var element = document.getElementsByTagName("Body");
-element[0].appendChild(para);
+var userLat,
+	userLong,
+	destLat,
+	destLong;
+
+var timer;
 
 
-var elements = document.getElementsByTagName("address");
-var i;
-for(i = 0; i < elements.length; i++){
-	var link = document.createElement("a");
-	link.href="http://www.google.com/search?tbm=isch&q=RED+PANDAAAA";
-	link.target="_blank";
-	var image = document.createElement("img");
-	var imageurl = chrome.extension.getURL("red.jpg");
-	image.src = imageurl;
-	image.width = "640";
-	image.height = "480";
-	link.appendChild(image);
-	elements[i].appendChild(link);
 
-	var para1 = document.createElement("p");
-	var node1 = document.createTextNode("RED PANDAAAA");
-	para1.appendChild(node1);
-}
+
+
+// var para = document.createElement("p");
+// var node = document.createTextNode("RED PANDAAAA");
+// para.appendChild(node);
+
+// var element = document.getElementsByTagName("Body");
+// element[0].appendChild(para);
+
+
+// var elements = document.getElementsByTagName("address");
+// var i;
+// for(i = 0; i < elements.length; i++){
+// 	var link = document.createElement("a");
+// 	link.href="http://www.google.com/search?tbm=isch&q=RED+PANDAAAA";
+// 	link.target="_blank";
+// 	var image = document.createElement("img");
+// 	var imageurl = chrome.extension.getURL("red.jpg");
+// 	image.src = imageurl;
+// 	image.width = "640";
+// 	image.height = "480";
+// 	link.appendChild(image);
+// 	elements[i].appendChild(link);
+
+// 	var para1 = document.createElement("p");
+// 	var node1 = document.createTextNode("RED PANDAAAA");
+// 	para1.appendChild(node1);
+// }
 
 //document.write("RED PANDAAAA");
