4f599db5d71b5063616e1bac0b7e16fb7fc7a0b8
lkuprenas <luke@kuprenas.com>
2015-09-19 23:17:02 -0400

    getting there

--- content.js
+++ content.js
@@ -1,3 +1,18 @@
+var elements = document.getElementsByTagName("address");
+var i;
+for(i = 0; i < elements.length; i++){
+	var link = document.createElement("a");
+	link.href = "#";
+	var container = document.createElement(div);
+	container.class = "button";
+	var time = document.createElement(p);
+	time.id = "time";
+	var node = document.createTextNode("ESTIMATING TIME");
+	time.appendChild(node);
+}
+
+
+
 var uberClientID = "1SgYLxvfNCPyUqS5s7uYZfB6AZlhdg49"
 	, serverToken = "Ks8Pt2MM9PGzf8WBW2ThZGZPmhBxFfkycbUSgyMk"
 	, gooToken = "AIzaSyCJhFwZi2GD3Qdsg7yiOLTv7cFa73FpDxI";
@@ -9,8 +24,69 @@ var userLat,
 
 var timer;
 
+navigator.geolocation.watchPosition(function(position) {
+	userLatitude = position.coords.latitude;
+	userLongitude = position.coords.longitude;
 
+	if (typeof timer === typeof undefined) {
+		timer = setInterval(function() {
+			getEstimatesForUserLocation(userLatitude, userLongitude);
+		}, 60000);
 
+		getEstimatesForUserLocation(userLatitude, userLongitude);
+	}
+});
+
+function getEstimatesForUserLocation(latitude,longitude) {
+	console.log("Requesting updated time estimate...");
+  $.ajax({
+    url: "https://api.uber.com/v1/estimates/price",
+    headers: {
+    	Authorization: "Token " + uberServerToken
+    },
+    data: { 
+    	start_latitude: latitude,
+    	start_longitude: longitude,
+    	end_latitude: partyLatitude,
+    	end_longitude: partyLongitude
+    },
+    success: function(result) {
+    	console.log(JSON.stringify(result));
+
+    	// 'results' is an object with a key containing an Array
+    	var data = result["prices"]; 
+    	if (typeof data != typeof undefined) {
+    		// Sort Uber products by time to the user's location 
+    		data.sort(function(t0, t1) {
+    			return t0.duration - t1.duration;
+    		});
+
+    		// Update the Uber button with the shortest time
+    		var shortest = data[0];
+    		if (typeof shortest != typeof undefined) {
+    			console.log("Updating time estimate...");
+					$("#time").html("IN " + Math.ceil(shortest.duration / 60.0) + " MIN");
+    		}
+    	}
+    }
+  });
+}
+
+$("a").click(function(event){
+	// Redirect to Uber API via deep-linking to the mobile web-app
+	var uberURL = "https://m.uber.com/sign-up?";
+
+	// Add parameters
+	uberURL += "client_id=" + uberClientId;
+	if (typeof userLatitude != typeof undefined) uberURL += "&" + "pickup_latitude=" + userLatitude;
+	if (typeof userLongitude != typeof undefined) uberURL += "&" + "pickup_longitude=" + userLongitude;
+	uberURL += "&" + "dropoff_latitude=" + partyLatitude;
+	uberURL += "&" + "dropoff_longitude=" + partyLongitude;
+	uberURL += "&" + "dropoff_nickname=" + "Thinkful";
+
+	// Redirect to Uber
+	window.location.href = uberURL;
+});
 
 
 // var para = document.createElement("p");
